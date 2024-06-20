3d0372410d99c9da3ef2f0347ee1c03e0a70702e
Kazuki H <kazukih0205@gmail.com>
2024-06-20 11:41:54 +0900

    Revert "Support compiling libjemalloc5 against musl"
    This reverts commit 7326a7c470d6506cd5eb5d1ea962dadded377a48.

--- include/jemalloc/internal/jemalloc_internal_defs_host.h
+++ include/jemalloc/internal/jemalloc_internal_defs_host.h
@@ -421,9 +421,7 @@
 /*
  * Defined if strerror_r returns char * if _GNU_SOURCE is defined.
  */
-#ifdef __GLIBC__
 #define JEMALLOC_STRERROR_R_RETURNS_CHAR_WITH_GNU_SOURCE 
-#endif
 
 /* Performs additional safety checks when defined. */
 /* #undef JEMALLOC_OPT_SAFETY_CHECKS */
